02e2d042a72971aca87746f1c98c1c190b97991e
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2024-11-10 13:09:53 -0600

    Menejo de errores y  configuracion de conexcion al servidor

--- Etl_ExamenI.py
+++ Etl_ExamenI.py
@@ -9,3 +9,18 @@ def cargar_datos():
         title="Seleccionar archivo Excel",
         filetypes=[("Archivos Excel", "*.xlsx")]
     )
+    if archivo_excel:
+         try:
+            # Cargar cada hoja del archivo Excel en DataFrames
+            df_pacientes = pd.read_excel(archivo_excel, sheet_name='Pacientes')
+            df_enfermeras = pd.read_excel(archivo_excel, sheet_name='Enfermeras')
+            df_consultas = pd.read_excel(archivo_excel, sheet_name='Consultas')
+            df_tratamientos = pd.read_excel(archivo_excel, sheet_name='Tratamientos')
+            
+            # Configurar la conexión a SQL Server
+            conexion = pyodbc.connect(
+                'DRIVER={SQL Server};'
+                'SERVER=DESKTOP-RCPI344;'
+                'DATABASE=ExamenEtl;'
+            )
+            cursor = conexion.cursor()
